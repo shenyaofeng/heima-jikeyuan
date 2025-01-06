320fb85ba278f50ce7d0500dbe3996cf9c74fa6f
hhh <hhh@qq.com>
2025-01-06 15:41:35 +0800

    fix:二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Layout from '../pages/Layout/index'
 
 // import Login from "../pages/Login/index";
 import Login from "@/pages/Login/index";
+import Home from '@/pages/Home';
+import Article from '@/pages/Article';
+import Publish from '@/pages/Publish';
 
 import { createBrowserRouter } from 'react-router-dom';
 import {AuthRoute} from "@/components/AuthRoute";
@@ -10,7 +13,21 @@ import {AuthRoute} from "@/components/AuthRoute";
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <AuthRoute><Layout/></AuthRoute>
+    element: <AuthRoute><Layout/></AuthRoute>,
+    children:[
+      {
+        path:'home',
+        element:<Home/>
+      },
+      {
+        path:'article',
+        element:<Article/>
+      },
+      {
+        path:'publish',
+        element:<Publish/>
+      }
+    ]
   },
   {
     path: '/login',
