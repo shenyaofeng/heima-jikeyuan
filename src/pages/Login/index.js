8e6beb527123171bb5487e40857c5c33651b21fc
hhh <hhh@qq.com>
2025-01-06 13:22:24 +0800

    fix:表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,11 +8,32 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              // 手机号校验
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+              }
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
