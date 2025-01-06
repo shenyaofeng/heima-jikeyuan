130386c5576ae4f6ee9437333a6f4b5b8639dee9
hhh <hhh@qq.com>
2025-01-06 19:27:36 +0800

    fix:二级路由相关配置

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -7,28 +7,34 @@ import {
 } from "@ant-design/icons";
 import "./index.scss";
 import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const { Header, Sider } = Layout;
 
 const items = [
   {
     label: "首页",
-    key: "1",
+    key: "/",
     icon: <HomeOutlined />,
   },
   {
     label: "文章管理",
-    key: "2",
+    key: "article",
     icon: <DiffOutlined />,
   },
   {
     label: "创建文章",
-    key: "3",
+    key: "publish",
     icon: <EditOutlined />,
   },
 ];
 
 const GeekLayout = () => {
+  const navigate = useNavigate();
+  const onMenuClick = (e) => {
+    console.log(e.key);
+    navigate(e.key)
+  }
   return (
     <Layout>
       <Header className="header">
@@ -50,6 +56,7 @@ const GeekLayout = () => {
             defaultSelectedKeys={["1"]}
             items={items}
             style={{ height: "100%", borderRight: 0 }}
+            onClick={(e) => onMenuClick(e)}
           ></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
